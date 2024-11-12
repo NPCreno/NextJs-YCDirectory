92219f82f697b898da9044a8ce67662aee019bd0
NPCreno <laurence.divinagracia25@gmail.com>
2024-11-12 18:47:23 +0800

    part7-Theming and Fonts

---
+++ layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const workSans = localFont({
+  src: [
+    { path: "./fonts/workSans-Black.ttf", weight: "900", style: "normal" },
+    { path: "./fonts/workSans-ExtraBold.ttf", weight: "800", style: "normal" },
+    { path: "./fonts/workSans-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/workSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/workSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/workSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/workSans-Thin.ttf", weight: "200", style: "normal" },
+    { path: "./fonts/workSans-ExtraLight.ttf", weight: "100", style: "normal" },
+  ],
+  variable: "--font-work-sans",
+});
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "YC Directory",
+  description: "Pitch, Vote and Grow",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={workSans.variable}>{children}</body>
+    </html>
+  );
+}
